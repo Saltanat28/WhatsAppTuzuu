e57070ccc569381ca7e0db9132a51918464e8681
saltanat <saltanatnematillakyzy6@gmail.com>
2022-12-12 17:57:49 +0600

    Initial commit

---
+++ src/impls/PersonServiceImpl.java
@@ -0,0 +1,19 @@
+package impls;
+
+import services.PersonService;
+
+public class PersonImpl implements PersonService {
+
+
+    @Override
+    public String createPassport() {
+        return null;
+    }
+
+    @Override
+    public String getPassportByFirstName() {
+        return null;
+    }
+
+
+}
